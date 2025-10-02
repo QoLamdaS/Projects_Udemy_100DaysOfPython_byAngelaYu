53c0e5d748bd0817194857f28aa2ed951c817b69
QoLamdaS <kaffeeunused@gmail.com>
2025-10-02 19:22:16 +0700

    Create a project by myself

---
+++ Day_1-100_Udemy-AngelaYu/Day1Project_BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the ;Band Name Generator;.\n")
+Name_City = input("What's the name of the city you grew up in?\n")
+Pet_Name = input("What's your pet's name?\n")
+print(f"Your band name could be {Name_City} {Pet_Name}")
