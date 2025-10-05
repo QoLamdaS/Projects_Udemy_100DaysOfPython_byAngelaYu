306a921b71cc7203ef72926854d378dfd2871b37
QoLamdaS <kaffeeunused@gmail.com>
2025-10-05 22:00:52 +0700

    Final revision, completed

--- Day_2-100/Day_2_Project_Tip_Calculator.py
+++ Day_2-100/Day_2_Project_Tip_Calculator.py
@@ -3,7 +3,7 @@ Total_Bill = float(input("What was the total bill? Rp"))
 TipPercentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 Person = int(input("How many people to split the bill? "))
 
-Person_real = (Total_Bill * ((100+TipPercentage) / 100)) / Person
-Person_Rounded = round(Person_real, 2)
+EachPerson = round((Total_Bill * ((100+TipPercentage) / 100)) / Person, 2)
+#Person_Rounded = round(Person_real, 2) [UNUSED]
 
-print(f"Each person should pay: Rp{Person_Rounded}")
+print(f"Each person should pay: Rp{EachPerson}")
