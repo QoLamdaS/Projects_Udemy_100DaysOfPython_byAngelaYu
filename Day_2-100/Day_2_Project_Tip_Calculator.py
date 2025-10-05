0e9bfeda13cff40b0e83a092d29566a78fd90592
QoLamdaS <kaffeeunused@gmail.com>
2025-10-05 21:50:19 +0700

    First prototype

--- Day_2-100/Day_2_Project_Tip_Calculator.py
+++ Day_2-100/Day_2_Project_Tip_Calculator.py
@@ -1,1 +1,9 @@
-print("Ich liebe die Pizza!")
+print("WELCOME di Kalkulator TIP!")
+Total_Bill = float(input("What was the total bill? Rp"))
+TipPercentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+Person = int(input("How many people to split the bill? "))
+
+Person_real = (Total_Bill + (TipPercentage / 100)) / Person
+Person_Rounded = round(Person_real, 2)
+
+print(f"Each person should pay: Rp{Person_Rounded}")
