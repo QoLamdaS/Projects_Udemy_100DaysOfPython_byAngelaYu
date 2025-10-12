988b0610dad0a58cdb24c7b8a7f8a10fe759000d
QoLamdaS <kaffeeunused@gmail.com>
2025-10-12 08:35:13 +0700

    Add my solution for Coding_Excercise_5

---
+++ Day3_100/Coding_Exercise_5; BMI Calculator with Interpretations.py
@@ -0,0 +1,14 @@
+weight = 85
+height = 1.85
+
+bmi = weight / (height ** 2)
+
+if bmi < 18.5:
+    print("Underweight")
+elif 18.5 <= bmi < 25:
+    print("normal weight")
+else:
+    print("OVERWEIGHT")
+    
+#! 🚨 Do not modify the values above
+#! Write your code below 👇
