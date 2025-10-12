7f1bcad8f7009fb8dc0e760e8fce582724bd13a4
QoLamdaS <kaffeeunused@gmail.com>
2025-10-12 13:45:00 +0700

    Add module random+os, rock-paper-scissor images, ComputerList, and If-Elif Statements. Finally completed this Day 4 Project.

--- Day_4-100/Day_4_Project;Rock Paper Scissor.py
+++ Day_4-100/Day_4_Project;Rock Paper Scissor.py
@@ -1,1 +1,66 @@
-print("ff")
+import random
+import os
+
+os.system("cls")
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+print("Rock Paper Scissor")
+Player_Input = input("Rock, Paper, or Scissor? ").upper()
+Computer_Input = random.choice(["rock", "paper", "scissor"])
+
+if Player_Input == "ROCK" and Computer_Input == "rock":
+    print(f"You: \n{rock}\nComputer:\n {rock}")
+    print("Draw!!!")
+elif Player_Input == "ROCK" and Computer_Input == "paper":
+    print(f"You: \n{rock}\nComputer:\n {paper}")
+    print("You Lose!!!")
+elif Player_Input == "ROCK" and Computer_Input == "scissor":
+    print(f"You: \n{rock}\nComputer:\n {scissors}")
+    print("You Win!!!")
+
+if Player_Input == "PAPER" and Computer_Input == "paper":
+    print(f"You: \n{paper}\nComputer:\n {paper}")
+    print("Draw!!!")
+elif Player_Input == "PAPER" and Computer_Input == "rock":
+    print(f"You: \n{paper}\nComputer:\n {rock}")
+    print("You Win!!!")
+elif Player_Input == "PAPER" and Computer_Input == "scissor":
+    print(f"You: \n{paper}\nComputer:\n {scissors}")
+    print("You Lose!!!")
+
+if Player_Input == "SCISSOR" and Computer_Input == "scissor":
+    print(f"You: \n{scissors}\nComputer:\n {scissors}")
+    print("Draw!!!")
+elif Player_Input == "SCISSOR" and Computer_Input == "rock":
+    print(f"You: \n{scissors}\nComputer:\n {rock}")
+    print("You Lose!!!")
+elif Player_Input == "SCISSOR" and Computer_Input == "paper":
+    print(f"You: \n{scissors}\nComputer:\n {paper}")
+    print("You Win!!!")
+
