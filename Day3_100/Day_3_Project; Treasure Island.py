2702a56b2e41bc3e045257f642b773fcef90400c
QoLamdaS <kaffeeunused@gmail.com>
2025-10-12 10:09:40 +0700

    Add all if-elif-else statements. This project finnally completed

--- Day3_100/Day_3_Project; Treasure Island.py
+++ Day3_100/Day_3_Project; Treasure Island.py
@@ -25,7 +25,20 @@ print('''
 
 print("\nWelcome to Treasure Island. Australian Version???\nYour mission is to find the Silver CHEST.")
 
-
-
+Left_or_Right = input("You're at a crossroad. Where do you want to go?\nType 'left' or 'right': ")
+if Left_or_Right.lower() == "left":
+    Wait_or_Swim = input("You come to a lake. There is an island in the middle of the lake.\nType 'wait' to wait for a boat. Type 'swim' to swim across: ")
+    if Wait_or_Swim.lower() == "wait":
+        Door_RedYellowBlue = input("You arrive at the island unharmed. There is a house with 3 doors.\nOne red, one yellow and one blue. Which color do you choose?: ")
+        if Door_RedYellowBlue.lower() == "red":
+            print("It's a room full of fire. Game Over.")
+        elif Door_RedYellowBlue.lower() == "yellow":
+            print("You found the Silver_CHEST! You Win!")
+        else:
+            print("DEATH. Game Over.")
+    else:
+        print("DEATH. Game Over.")
+else:
+    print("DEATH. Game Over.")
 
 
