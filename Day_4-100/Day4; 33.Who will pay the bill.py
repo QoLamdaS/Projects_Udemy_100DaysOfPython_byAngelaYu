99747de1d013afff921b6474d125998712288e99
QoLamdaS <kaffeeunused@gmail.com>
2025-10-12 12:33:06 +0700

    Add import random and a Friends variable list. This challenge completed!!!

--- Day_4-100/Day4; 33.Who will pay the bill.py
+++ Day_4-100/Day4; 33.Who will pay the bill.py
@@ -1,1 +1,5 @@
-print("wse")
+import random
+
+Friends = ["Charles", "Bob", "Alice", "Henry", "Maggie", "Ella", "Mia", "Zoe"]
+
+print(f"{random.choice(Friends)} is going to buy/'TRAKTIR' the meal today!")
