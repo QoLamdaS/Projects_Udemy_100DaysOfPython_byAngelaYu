d3d79b044e809fdc60814302589a34b8460e8ca3
QoLamdaS <kaffeeunused@gmail.com>
2025-10-19 10:22:26 +0700

    Add int variables input, for loops, random modules. Finally finished this project Day 5. =)

--- Day_5-100/Day_5_Project_Create a Password Generator.py
+++ Day_5-100/Day_5_Project_Create a Password Generator.py
@@ -1,9 +1,22 @@
 import string
 import random
 
+print("Welcome to the Py_Password Generator my VERSION!")
+NumLetters = int(input("How many letters would you like in your password? "))
+NumSymbols = int(input("How many symbols would you like in your password? "))
+NumDigits = int(input("How many digits would you like in your password? "))
+
 all_digitsWhole = list(string.digits)
 all_symbols = list(string.punctuation)
 all_letters = list(string.ascii_letters)
-print(all_letters)
-print(all_symbols)
-print(all_digitsWhole)
+password = []
+for i in range(NumLetters):
+    password.append(random.choice(all_letters))
+for i in range(NumSymbols):
+    password.append(random.choice(all_symbols))
+for i in range(NumDigits):
+    password.append(random.choice(all_digitsWhole))
+
+random.shuffle(password)
+password = "".join(password)
+print(f"Your new generated password is: {password}")
