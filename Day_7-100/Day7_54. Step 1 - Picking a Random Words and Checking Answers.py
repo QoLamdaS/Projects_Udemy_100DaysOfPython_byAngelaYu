1bcfc78df4045378494c2a411e2d62f59c13f160
QoLamdaS <kaffeeunused@gmail.com>
2025-11-01 14:41:59 +0700

    Finally completed this a challenge Day7_54. (It's simple lol)

--- Day_7-100/Day7_54. Step 1 - Picking a Random Words and Checking Answers.py
+++ Day_7-100/Day7_54. Step 1 - Picking a Random Words and Checking Answers.py
@@ -3,4 +3,10 @@ import random, os
 os.system("cls")
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-print(chosen_word)
+input_guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == input_guess:
+        print("Right")
+    else:
+        print("Wrong")
