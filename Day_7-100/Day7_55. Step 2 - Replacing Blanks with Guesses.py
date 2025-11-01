f7a51636c58f3e52e022f363e349ee2eddf5a298
QoLamdaS <kaffeeunused@gmail.com>
2025-11-01 22:52:42 +0700

    Add from previous file "Day7_54. "

--- Day_7-100/Day7_55. Step 2 - Replacing Blanks with Guesses.py
+++ Day_7-100/Day7_55. Step 2 - Replacing Blanks with Guesses.py
@@ -1,1 +1,12 @@
-print("dcd")
+import random, os
+
+os.system("cls")
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+input_guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == input_guess:
+        print("Right")
+    else:
+        print("Wrong")
