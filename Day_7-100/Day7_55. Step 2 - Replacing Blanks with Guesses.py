b744270473b9d856d15ebba2a61f53962d605cc4
QoLamdaS <kaffeeunused@gmail.com>
2025-11-02 09:01:06 +0700

    Add Underscores for randomized chosen_word in Day7_55. file

--- Day_7-100/Day7_55. Step 2 - Replacing Blanks with Guesses.py
+++ Day_7-100/Day7_55. Step 2 - Replacing Blanks with Guesses.py
@@ -1,8 +1,13 @@
-import random, os
+import random
+import os
 
 os.system("cls")
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+chosen_word = list(chosen_word)
+for i in range(len(chosen_word)):
+    chosen_word[i] = "_"
+print(" ".join(chosen_word))
 input_guess = input("Guess a letter: ").lower()
 
 for letter in chosen_word:
