414a778d0acbf9023ef6aa25983231452393fec2
QoLamdaS <kaffeeunused@gmail.com>
2025-11-02 11:41:27 +0700

    Add code from previous file to this file "Day7_56. "

--- Day_7-100/Day7_56. Step 3 - Checking if the Player has Won.py
+++ Day_7-100/Day7_56. Step 3 - Checking if the Player has Won.py
@@ -1,1 +1,19 @@
-print("dfd")
+import random
+import os
+
+os.system("cls")
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+chosen_word_underscores = list(chosen_word)
+for i in range(len(chosen_word)):
+    chosen_word_underscores[i] = "_"
+print(" ".join(chosen_word_underscores))
+input_guess = input("Guess a letter: ").lower()
+
+display = []
+for letter in chosen_word:
+    if letter == input_guess:
+        display.append(input_guess)
+    else:
+        display.append("_")
+print(" ".join(display))
