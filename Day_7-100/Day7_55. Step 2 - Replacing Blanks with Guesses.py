b09e935dd4ffdf7c0f604fd8c8e4d00f0c2c3fd6
QoLamdaS <kaffeeunused@gmail.com>
2025-11-02 09:17:38 +0700

    Finally completed challenge for Day7_55. file. Add display user by for loop and fix "The Underscores" (line 7, 9, and 10).

--- Day_7-100/Day7_55. Step 2 - Replacing Blanks with Guesses.py
+++ Day_7-100/Day7_55. Step 2 - Replacing Blanks with Guesses.py
@@ -4,14 +4,16 @@ import os
 os.system("cls")
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-chosen_word = list(chosen_word)
+chosen_word_underscores = list(chosen_word)
 for i in range(len(chosen_word)):
-    chosen_word[i] = "_"
-print(" ".join(chosen_word))
+    chosen_word_underscores[i] = "_"
+print(" ".join(chosen_word_underscores))
 input_guess = input("Guess a letter: ").lower()
 
+display = []
 for letter in chosen_word:
     if letter == input_guess:
-        print("Right")
+        display.append(input_guess)
     else:
-        print("Wrong")
+        display.append("_")
+print(" ".join(display))
