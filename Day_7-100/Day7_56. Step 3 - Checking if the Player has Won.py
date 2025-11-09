ecad969864692f93d20111a27f19dc16be1e6000
QoLamdaS <kaffeeunused@gmail.com>
2025-11-09 09:34:34 +0700

    Literally copy-paste from "Day7_56. " Dr. Yu video Udemy

--- Day_7-100/Day7_56. Step 3 - Checking if the Player has Won.py
+++ Day_7-100/Day7_56. Step 3 - Checking if the Player has Won.py
@@ -10,13 +10,23 @@ for position in range(word_length):
     placeholder += "_"
 print (placeholder)
 
-guess = input("Guess a letter: ").lower()
+correct_letters = []
+game_over = False
 
-display = ""
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+    display = ""
 
-for letter in chosen_word:
-    if letter == guess:
-        display += letter
-    else:
-        display += "_"
-print(display)
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+
+    if "_" not in display:
+        game_over = True
+        print("You win!")
