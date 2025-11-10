66ad250172d3f98d65bfbeefdeabb0c9431256f7
QoLamdaS <kaffeeunused@gmail.com>
2025-11-10 20:20:21 +0700

    Add new file for "Day7_57. " and copy-paste code completely from previous file (Day7_56. ). =)

---
+++ Day_7-100/Day7_57. Step 4 - Keeping Track of the Player's Lives.py
@@ -0,0 +1,35 @@
+import random
+import os
+
+os.system("cls")
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print (placeholder)
+
+correct_letters = []
+
+while True:
+    guess = input("Guess a letter: ").lower()
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+
+    if "_" not in display:
+        os.system("cls")
+        break
+
+print("\nYEAH, You win!!!!!!!!!!!!\n")
