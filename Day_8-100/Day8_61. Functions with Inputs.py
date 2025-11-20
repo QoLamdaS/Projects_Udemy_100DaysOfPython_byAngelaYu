ec38758d82aa7a4546dc0ab03e3cd379925852dd
QoLamdaS <kaffeeunused@gmail.com>
2025-11-20 17:06:56 +0700

    Add new file "Day8_61. " file challenge and solved by my own. Feel easy =)

---
+++ Day_8-100/Day8_61. Functions with Inputs.py
@@ -0,0 +1,12 @@
+
+def challenge():
+    first_challenge = print("\nfirst\nsecond\nthird\n")
+    return first_challenge
+
+print_first_challenge = challenge()
+
+def greet(name):
+    message = f"Hello, {name}!\n"
+    return message
+
+greeting_message = print(greet("Alice"))
