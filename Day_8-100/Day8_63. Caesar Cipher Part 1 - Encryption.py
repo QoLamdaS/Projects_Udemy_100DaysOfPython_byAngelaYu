e6be2466feb1243a31db6cb73adf8a7c061568b8
QoLamdaS <kaffeeunused@gmail.com>
2025-11-22 11:18:01 +0700

    Add necessary user input from first challenge "Day8_63. "

--- Day_8-100/Day8_63. Caesar Cipher Part 1 - Encryption.py
+++ Day_8-100/Day8_63. Caesar Cipher Part 1 - Encryption.py
@@ -1,3 +1,8 @@
 import string
 
 lower_alphabet = list(string.ascii_lowercase)
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
