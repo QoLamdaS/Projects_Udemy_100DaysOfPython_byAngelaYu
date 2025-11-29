dbd8ba63a30b9553f2bab80326b29b64d21d5427
QoLamdaS <kaffeeunused@gmail.com>
2025-11-29 14:34:26 +0700

    Add solutions from Windsurf?? I really have no idea

--- Day_8-100/Day8_63. Caesar Cipher Part 1 - Encryption.py
+++ Day_8-100/Day8_63. Caesar Cipher Part 1 - Encryption.py
@@ -8,4 +8,23 @@ shift = int(input("Type the shift number:\n"))
 
 def caesar(original_text, shift_amount, cipher_direction):
     end_text = ""
-    
+    if cipher_direction == "encode":
+        for char in original_text:
+            if char in lower_alphabet:
+                position = lower_alphabet.index(char)
+                new_position = (position + shift_amount) % 26
+                end_text += lower_alphabet[new_position]
+            else:
+                end_text += char
+    elif cipher_direction == "decode":
+        for char in original_text:
+            if char in lower_alphabet:
+                position = lower_alphabet.index(char)
+                new_position = (position - shift_amount) % 26
+                end_text += lower_alphabet[new_position]
+            else:
+                end_text += char
+    return end_text
+
+result = caesar(original_text=text, shift_amount=shift, cipher_direction=direction)
+print(f"The {direction}d text is: {result}")
