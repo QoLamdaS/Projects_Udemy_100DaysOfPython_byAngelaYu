4856fd31b73434a895e41a33dba24fddc95fb35c
QoLamdaS <kaffeeunused@gmail.com>
2025-12-07 12:48:00 +0700

    Solved Coding Exercise 9 in Day 9 by my own.

--- Day_9-100/Coding Exercise 9; Grading Program.py
+++ Day_9-100/Coding Exercise 9; Grading Program.py
@@ -6,5 +6,15 @@ student_scores = {
     'Draco': 75,
     'Neville': 60
 }
+for key in student_scores:
+    if student_scores[key] >= 91:
+        student_scores[key] = "Outstanding"
+    elif student_scores[key] >= 81:
+        student_scores[key] = "Exceeds Expectations"
+    elif student_scores[key] >= 71:
+        student_scores[key] = "Acceptable"
+    else:
+        student_scores[key] = "Fail"
 
-student_grades =
+student_grades = student_scores
+print(student_grades)
