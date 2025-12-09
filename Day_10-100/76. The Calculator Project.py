5c9cc1505b48de517a3df4e2bdcd70a37d4aa7d7
QoLamdaS <kaffeeunused@gmail.com>
2025-12-09 13:52:45 +0700

    Finally finished Day 10 Project building calculator program. Btw my own solutions actually/seem easier than Dr. Angela suprisingly hehehehe =) Day 10 Course finally completed

--- Day_10-100/76. The Calculator Project.py
+++ Day_10-100/76. The Calculator Project.py
@@ -46,5 +46,20 @@ while True:
 | |___|___|___| |___| |
 |_____________________|
 
-'''
+    '''
     print(logo)
+    user_num1 = float(input("What is your the first number?: "))
+    while True:
+        for symbol in mathematical_operations:
+            print(symbol)
+        user_symbol = input("Pick an operation from the line above to calculate: ")
+        user_next_num = float(input("What is your the next number?: "))
+        
+        calculated_result = mathematical_operations[user_symbol](n1=user_num1, n2=user_next_num)
+        
+        print(f"{user_num1} {user_symbol} {user_next_num} = {calculated_result}")
+        if input(f"Type 'yes' to continue calculating with {calculated_result}, or type 'no' to start a new calculation.: ").lower() == 'yes':
+            user_num1 = calculated_result
+            continue
+        else:
+            break
