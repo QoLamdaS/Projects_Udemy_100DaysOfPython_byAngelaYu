42a2c2d740d1c0f794f4abab60b5f219ffe9773f
QoLamdaS <kaffeeunused@gmail.com>
2025-12-09 18:23:00 +0700

    Add if-else to play blackjack asking the user very first time

--- Day_11-100/The Blackjack Capstone Project.py
+++ Day_11-100/The Blackjack Capstone Project.py
@@ -1,3 +1,7 @@
 import os
 
 os.system("cls")
+if input("Are you ready to play Blackjack? Type 'yes' or 'no': ").lower() == "yes":
+    print("testing only")
+else:
+    print("\nMaybe next time!\nThank you so much!\n")
