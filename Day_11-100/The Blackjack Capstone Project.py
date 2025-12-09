5106adeee9e22233a731a286f5ff7dcf5a365d66
QoLamdaS <kaffeeunused@gmail.com>
2025-12-09 18:24:18 +0700

    Import sys module exit() for else statement firstly

--- Day_11-100/The Blackjack Capstone Project.py
+++ Day_11-100/The Blackjack Capstone Project.py
@@ -1,7 +1,9 @@
 import os
+import sys
 
 os.system("cls")
 if input("Are you ready to play Blackjack? Type 'yes' or 'no': ").lower() == "yes":
     print("testing only")
 else:
     print("\nMaybe next time!\nThank you so much!\n")
+    sys.exit()
