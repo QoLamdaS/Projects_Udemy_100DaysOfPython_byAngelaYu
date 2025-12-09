6dfd5c25d6c8e3dfc07dda914f1e55db1a68c71a
QoLamdaS <kaffeeunused@gmail.com>
2025-12-09 19:54:34 +0700

    Improve UX for very first user input if-elif-else statement early

--- Day_11-100/The Blackjack Capstone Project.py
+++ Day_11-100/The Blackjack Capstone Project.py
@@ -2,8 +2,12 @@ import os
 import sys
 
 os.system("cls")
-if input("Are you ready to play Blackjack? Type 'yes' or 'no': ").lower() == "yes":
+user_wants_to_play = input("Are you ready to play Blackjack? Type 'yes' or 'no': ").lower()
+if user_wants_to_play == "yes" or user_wants_to_play == "y":
     print("testing only")
-else:
+elif user_wants_to_play == "no" or user_wants_to_play == "n":
     print("\nMaybe next time!\nThank you so much!\n")
+    sys.exit()
+else:
+    print("CRITICAL ERROR. Invalid input. Please restart the game and type 'yes' or 'no' only please.")
     sys.exit()
