63f46e1099eb05044f411dd77141b0fdda503570
QoLamdaS <kaffeeunused@gmail.com>
2025-12-10 18:56:25 +0700

    Add hint 6-8, I peak a bit from dr angela solution hehehe =)

--- Day_11-100/The Blackjack Capstone Project.py
+++ Day_11-100/The Blackjack Capstone Project.py
@@ -6,6 +6,14 @@ def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return random.choice(cards)
 
+def score_calculation(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
 user_card = {}
 comp_dealer_card = {}
 for i in range(2):
