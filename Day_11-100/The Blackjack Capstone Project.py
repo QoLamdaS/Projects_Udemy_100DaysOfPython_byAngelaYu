98c380dbef331087d3a381e029d5e5af47e4cbbb
QoLamdaS <kaffeeunused@gmail.com>
2025-12-10 19:13:16 +0700

    Honestly I solve hint 8 and 9 but still no idea =)

--- Day_11-100/The Blackjack Capstone Project.py
+++ Day_11-100/The Blackjack Capstone Project.py
@@ -3,10 +3,12 @@ import sys
 import random
 
 def deal_card():
+    '''Random early cards generator'''
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return random.choice(cards)
 
 def score_calculation(cards):
+    '''Blackjack score calculation, rules translated to Py code'''
     if sum(cards) == 21 and len(cards) == 2:
         return 0
     if 11 in cards and sum(cards) > 21:
@@ -19,6 +21,9 @@ comp_dealer_card = {}
 for i in range(2):
     user_card.append(deal_card())
     comp_dealer_card.append(deal_card())
+
+user_score = score_calculation(user_card)
+comp_dealer_score = score_calculation(comp_dealer_card)
 
 os.system("cls")
 user_wants_to_play = input("Are you ready to play Blackjack? Type 'yes' or 'no': ").lower()
