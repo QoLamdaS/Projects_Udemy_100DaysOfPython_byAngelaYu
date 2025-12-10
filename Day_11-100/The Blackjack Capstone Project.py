49f8be37d102657f1f1940cab64e06816b1dcdb1
QoLamdaS <kaffeeunused@gmail.com>
2025-12-10 20:55:51 +0700

    I "give up" to fix minor bugs/errors (95% finished), I feel 'burnout' and exhausted to work this project. Okay the most importantly, I understand the core logic this game and also I grasp all core concepts so far. I feel exhausted =)

--- Day_11-100/The Blackjack Capstone Project.py
+++ Day_11-100/The Blackjack Capstone Project.py
@@ -16,14 +16,31 @@ def score_calculation(cards):
         cards.append(1)
     return sum(cards)
 
-user_card = {}
-comp_dealer_card = {}
-for i in range(2):
-    user_card.append(deal_card())
-    comp_dealer_card.append(deal_card())
+def compare(user_score, comp_dealer_score):
+    """Compare user and computer dealer scores and declare the winner"""
+    if user_score == comp_dealer_score:
+        return "It's a Draw!"
+    elif comp_dealer_score == 0:
+        return "You Lose! Computer dealer has a Blackjack!"
+    elif user_score == 0:
+        return "You Win with a Blackjack!"
+    elif user_score > 21:
+        return "You went over. You Lose!"
+    elif comp_dealer_score > 21:
+        return "Computer dealer went over. You Win!"
+    elif user_score > comp_dealer_score:
+        return "YOU WIN!!!"
+    else:
+        return "YOU LOSE!!!"
 
-user_score = score_calculation(user_card)
-comp_dealer_score = score_calculation(comp_dealer_card)
+# user_card = []
+# comp_dealer_card = []
+# for i in range(2):
+#     user_card.append(deal_card())
+#     comp_dealer_card.append(deal_card())
+
+# user_score = score_calculation(user_card)
+# comp_dealer_score = score_calculation(comp_dealer_card)
 
 os.system("cls")
 user_wants_to_play = input("Are you ready to play Blackjack? Type 'yes' or 'no': ").lower()
@@ -40,7 +57,38 @@ if user_wants_to_play == "yes" or user_wants_to_play == "y":
                       |__/           
 """
         print(logo)
+        
+        user_card = []
+        comp_dealer_card = []
+        for i in range(2):
+            user_card.append(deal_card())
+            comp_dealer_card.append(deal_card())
+        user_score = score_calculation(user_card)
+        comp_dealer_score = score_calculation(comp_dealer_card)
 
+        while True:
+            print(f"Your cards: {user_card} ; and your current score: {user_score}") # To show all the cards and first score of the user from player POV
+            print(f"Computer dealer's first card: {comp_dealer_card[0]}") # To show only the first card of the computer_dealer from player POV
+            user_choice = input("Type 'hit' to get another card, type 'pass' to pass: ").lower()
+            if user_choice == "hit":
+                user_card.append(deal_card())
+            else:
+                break
+        user_score = score_calculation(user_card)
+        while comp_dealer_score < 17:
+            comp_dealer_card.append(deal_card())
+            comp_dealer_score = score_calculation(comp_dealer_card)
+
+        print(f"Your final hand: {user_card} ; and your final score: {user_score}")
+        print(f"Computer dealer's final hand: {comp_dealer_card} ; and computer dealer's final score: {comp_dealer_score}")
+        print(compare(user_score, comp_dealer_score))
+        play_again = input("\nDo you want to play a new game of Blackjack? Type 'yes' or 'no': ").lower()
+        if play_again == "yes" or play_again == "y":
+            os.system("cls")
+            continue
+        else:
+            print("\nThank you so much for playing! Goodbye!\n")
+            sys.exit()
 
 elif user_wants_to_play == "no" or user_wants_to_play == "n":
     print("\nMaybe next time!\nThank you so much!\n")
