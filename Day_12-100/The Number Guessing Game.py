4f91b10281c8404af92fac6513b823255642ceae
QoLamdaS <kaffeeunused@gmail.com>
2025-12-14 14:33:05 +0700

    if-else statement for difficulty

--- Day_12-100/The Number Guessing Game.py
+++ Day_12-100/The Number Guessing Game.py
@@ -2,10 +2,15 @@ import logo_TheNumberGuessingGame
 import os
 import random
 
-os.system('cls' if os.name == 'nt' else 'clear')
-print(logo_TheNumberGuessingGame.the_logo)
-
 def the_answer():
     return random.randint(1, 100)
 
+os.system('cls' if os.name == 'nt' else 'clear')
+print(logo_TheNumberGuessingGame.the_logo)
+print("Welcome to The Number Guessing Game! YEAH!!!!!!")
+
+if input("Choose a difficulty. Type 'easy' or 'hard': ").lower() == "hard":
+    print("difficult")
+else:
+    print("easy")
 
