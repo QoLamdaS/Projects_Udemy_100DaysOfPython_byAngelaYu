d44fb48c2b5314731c3febbf6af79c410640ce3c
QoLamdaS <kaffeeunused@gmail.com>
2025-12-14 13:35:35 +0700

    Add function to generate random number

--- Day_12-100/The Number Guessing Game.py
+++ Day_12-100/The Number Guessing Game.py
@@ -1,5 +1,11 @@
 import logo_TheNumberGuessingGame
 import os
+import random
 
 os.system('cls' if os.name == 'nt' else 'clear')
 print(logo_TheNumberGuessingGame.the_logo)
+
+def the_answer():
+    return random.randint(1, 100)
+
+
