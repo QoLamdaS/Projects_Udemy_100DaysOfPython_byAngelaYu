efec267a5b231102e9cff6e5e413eabcc1f83249
QoLamdaS <kaffeeunused@gmail.com>
2025-12-16 19:47:54 +0700

    Have some problems with VScode about this code. I should find the solution outside Dr. Angela Course

--- Day_16-100/OOP Concept Testing File.py
+++ Day_16-100/OOP Concept Testing File.py
@@ -1,2 +1,14 @@
+# import another_module
+# print(another_module.another_variable)
 
-print("ff")
+from turtle import Turtle, Screen
+timmy = Turtle()
+
+print(timmy)
+timmy.shape("turtle")
+timmy.color ("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
