3518499aed0bc022a7b983ea762660b0a8866a45
QoLamdaS <kaffeeunused@gmail.com>
2025-12-17 12:12:09 +0700

    Updated to reveiew OOP understandings

--- Day_16-100/OOP Concept Testing File.py
+++ Day_16-100/OOP Concept Testing File.py
@@ -1,17 +1,17 @@
-# import another_module
-# print(another_module.another_variable)
+import another_module
+print(another_module.another_variable)
 
-# from turtle import Turtle, Screen
-# tim = Turtle()
+from turtle import Turtle, Screen
+tim = Turtle()
 
-# print(tim)
-# tim.shape("turtle")
-# tim.color ("coral")
-# tim.forward(100)
+print(tim)
+tim.shape("turtle")
+tim.color ("coral")
+tim.forward(100)
 
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
 
 from prettytable import PrettyTable
 table = PrettyTable()
