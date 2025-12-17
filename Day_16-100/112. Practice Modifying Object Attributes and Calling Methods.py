6d848bbe53ca75b335f6fa725508d1085e4f8ded
QoLamdaS <kaffeeunused@gmail.com>
2025-12-17 09:19:00 +0700

    Solve second challenge easily

--- Day_16-100/112. Practice Modifying Object Attributes and Calling Methods.py
+++ Day_16-100/112. Practice Modifying Object Attributes and Calling Methods.py
@@ -1,7 +1,6 @@
 
 from prettytable import PrettyTable
 table = PrettyTable()
-table.add_column('Name', ['Jade', 'John', 'Jane'])
-table.add_column('Age', [20, 30, 40])
-table.add_column('Results', [9.651, 3, 245.7])
+table.add_column('Pokemon Name', ['Pikachu', 'Squirtle', 'Charmander'])
+table.add_column('Type', ['Electric', 'Water', 'Fire'])
 print(table)
