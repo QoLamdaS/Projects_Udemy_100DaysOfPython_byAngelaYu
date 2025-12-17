c94fbad2d1fed3f17c1cb9236159ddbc2921db87
QoLamdaS <kaffeeunused@gmail.com>
2025-12-17 09:15:42 +0700

    Add template to solve second challenge

--- Day_16-100/112. Practice Modifying Object Attributes and Calling Methods.py
+++ Day_16-100/112. Practice Modifying Object Attributes and Calling Methods.py
@@ -1,4 +1,7 @@
 
 from prettytable import PrettyTable
 table = PrettyTable()
+table.add_column('Name', ['Jade', 'John', 'Jane'])
+table.add_column('Age', [20, 30, 40])
+table.add_column('Results', [9.651, 3, 245.7])
 print(table)
