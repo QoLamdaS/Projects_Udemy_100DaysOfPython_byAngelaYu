b1c6dcbbfcf246e2217c1ddbd1e1f8ded4aa809a
QoLamdaS <kaffeeunused@gmail.com>
2025-12-17 12:03:29 +0700

    Add very early setup

--- Day_16-100/The Coffee Machine Program OOP Version Project/main.py
+++ Day_16-100/The Coffee Machine Program OOP Version Project/main.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+os.system('cls' if os.name == 'nt' else 'clear')
+print("Welcome to the 'VIRTUAL' Coffee Machine Program! OOP VERSION!!!!\n")
+
+
