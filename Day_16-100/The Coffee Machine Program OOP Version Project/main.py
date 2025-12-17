72d00a25beed30fd44acd54781dc85ac06a4eab7
QoLamdaS <kaffeeunused@gmail.com>
2025-12-17 12:58:09 +0700

    Finally completed Day 16 Project relatively easily. I understand the code. So far, I find zero bug for the current code this file

--- Day_16-100/The Coffee Machine Program OOP Version Project/main.py
+++ Day_16-100/The Coffee Machine Program OOP Version Project/main.py
@@ -7,5 +7,20 @@ from money_machine import MoneyMachine
 
 os.system('cls' if os.name == 'nt' else 'clear')
 print("Welcome to the 'VIRTUAL' Coffee Machine Program! OOP VERSION!!!!\n")
+main_menu = Menu()
+main_coffee_maker = CoffeeMaker()
+main_money_machine = MoneyMachine()
 
-
+while True:
+    user_wants = input("What would you like? (espresso/latte/cappuccino) or maybe 'off' and 'report': ").lower()
+    if user_wants == "off":
+        print("\nTurning off the coffee machine. Goodbye!\n")
+        sys.exit()
+    elif user_wants == "report":
+        main_coffee_maker.report()
+        main_money_machine.report()
+    else:
+        the_drink = main_menu.find_drink(user_wants)
+        if main_coffee_maker.is_resource_sufficient(the_drink):
+            if main_money_machine.make_payment(the_drink.cost):
+                main_coffee_maker.make_coffee(the_drink)
