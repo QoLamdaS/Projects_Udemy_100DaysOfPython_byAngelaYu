5d25485e8936a470c2e8c25be47cb21cd7b8281e
QoLamdaS <kaffeeunused@gmail.com>
2025-12-18 12:06:37 +0700

    Solve 123. challenge. I have no idea what Dr. Angela talking about for the challenge =). I half-understanded for my code

--- Day_17-100/Quiz Game Project/quiz_brain.py
+++ Day_17-100/Quiz Game Project/quiz_brain.py
@@ -2,6 +2,9 @@ class QuizBrain :
     def __init__(self, question_list):
         self.q_number = 0
         self.q_list = question_list
+    
+    def still_has_questions(self):
+        return self.q_number < len(self.q_list)
 
     def next_question(self):
         current_question = self.q_list[self.q_number]
