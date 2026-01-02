6e184a88883ca3dfe09b03efe2cb5e9197a2bb30
QoLamdaS <kaffeeunused@gmail.com>
2026-01-02 14:44:13 +0700

    Add Screen class and exitnonclick() feature. I COMEBACK PROGRAMMING YEAY!!

--- Day_18-100/Turtle Challenges Project/main.py
+++ Day_18-100/Turtle Challenges Project/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+from turtle import Screen
 
 a_turtle = Turtle()
 a_turtle.shape("turtle")
@@ -6,3 +7,6 @@ a_turtle.color("red4")
 for _ in range(4):
     a_turtle.forward(100)
     a_turtle.right(90)
+
+the_screen = Screen()
+the_screen.exitonclick()
