a8f429c65c89be14cc63e5134422fa9896d248b5
QoLamdaS <kaffeeunused@gmail.com>
2026-01-04 15:36:07 +0700

    Almost solved the challenge of Lecture 134.

--- Day_18-100/Turtle Challenges Project/main.py
+++ Day_18-100/Turtle Challenges Project/main.py
@@ -1,15 +1,22 @@
 import turtle
 import random
 
+def random_colors():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
 directions = [0, 90, 180, 270]
-colors = ["red", "blue", "green", "yellow", "purple", "orange"]
 a_turtle = turtle.Turtle()
 the_screen = turtle.Screen()
 a_turtle.shape("turtle")
 a_turtle.pensize(25)
 a_turtle.speed(0)
+
 for _ in range(200):
-    a_turtle.color(random.choice(colors))
+    a_turtle.color(random_colors()) #? ERROR, I don't know why
     a_turtle.setheading(random.choice(directions))
     a_turtle.forward(30)
 
