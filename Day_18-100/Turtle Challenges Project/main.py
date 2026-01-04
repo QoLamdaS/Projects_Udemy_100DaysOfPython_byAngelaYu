5889144f8cc03745d711d1f3973dbed82fa46bd7
QoLamdaS <kaffeeunused@gmail.com>
2026-01-04 15:24:53 +0700

    Solved 133. Challenge by creating random walk Python Turtle

--- Day_18-100/Turtle Challenges Project/main.py
+++ Day_18-100/Turtle Challenges Project/main.py
@@ -1,8 +1,17 @@
 import turtle
 import random
 
+directions = [0, 90, 180, 270]
+colors = ["red", "blue", "green", "yellow", "purple", "orange"]
 a_turtle = turtle.Turtle()
 the_screen = turtle.Screen()
 a_turtle.shape("turtle")
+a_turtle.pensize(25)
+a_turtle.speed(0)
+for _ in range(200):
+    a_turtle.color(random.choice(colors))
+    a_turtle.setheading(random.choice(directions))
+    a_turtle.forward(30)
+
 
 the_screen.mainloop()
