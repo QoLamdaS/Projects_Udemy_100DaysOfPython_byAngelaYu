98d4ae8f6f4a09627658a32f3f41674a1e1ffbb8
QoLamdaS <kaffeeunused@gmail.com>
2026-01-05 18:46:38 +0700

    Finally the error fixed and completed Lecture 134 Day 18 Challenge

--- Day_18-100/Turtle Challenges Project/main.py
+++ Day_18-100/Turtle Challenges Project/main.py
@@ -1,6 +1,7 @@
 import turtle
 import random
 
+turtle.colormode(255) #! Tell turtle to accept RGB values from 0–255
 def random_colors():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
@@ -16,7 +17,7 @@ a_turtle.pensize(25)
 a_turtle.speed(0)
 
 for _ in range(200):
-    a_turtle.color(random_colors()) #? ERROR, I don't know why
+    a_turtle.color(random_colors()) # NOW WORKS!!!!!!!
     a_turtle.setheading(random.choice(directions))
     a_turtle.forward(30)
 
