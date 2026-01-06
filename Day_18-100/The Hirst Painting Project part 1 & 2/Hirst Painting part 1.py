b25e4e4b54c076194530c62f50bdf53149dd9f80
QoLamdaS <kaffeeunused@gmail.com>
2026-01-06 18:00:46 +0700

    Solved Lecture 136 Day 18 Project part 1 finally. I feel sick and can't focus today.

--- Day_18-100/The Hirst Painting Project part 1 & 2/Hirst Painting part 1.py
+++ Day_18-100/The Hirst Painting Project part 1 & 2/Hirst Painting part 1.py
@@ -1,20 +1,13 @@
 import colorgram
 
-# Extract 6 colors from an image
-colors = colorgram.extract('D:\downloadrandom\Projects_Udemy_100DaysOfPython_byAngelaYu\Day_18-100\The Hirst Painting Project part 1 & 2\hirst_painting_dots.jpg', 6)
-
-# Access the first color
-first_color = colors[0]
-rgb = first_color.rgb 
-
-# Get the R, G, B components
-r = rgb.r
-g = rgb.g
-b = rgb.b
-
-# Print the RGB values
-print(f"R: {r}, G: {g}, B: {b}")
-
-# You can also iterate through all the colors
+rgb_colors = []
+colors = colorgram.extract('D:\downloadrandom\Projects_Udemy_100DaysOfPython_byAngelaYu\Day_18-100\The Hirst Painting Project part 1 & 2\hirst_painting_dots.jpg', 30)
 for color in colors:
-    print(f"RGB: {color.rgb}, Proportion: {color.proportion:.2f}%")
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+print(rgb_colors)
+
+actual_colors = [(19, 24, 48), (197, 139, 153), (136, 67, 91), (224, 207, 135), (74, 96, 129), (91, 113, 100), (198, 87, 108), (138, 157, 149), (60, 20, 35), (146, 157, 175), (117, 36, 59), (225, 168, 184), (39, 56, 103), (131, 82, 64), (221, 182, 168), (104, 126, 159), (110, 138, 126), (182, 189, 209), (38, 80, 59), (183, 198, 184), (173, 114, 95), (39, 69, 53), (212, 115, 37), (102, 51, 28), (113, 138, 142)]
