8128b7352b73ec81718a1695a5cfe08ecbe56c48
QoLamdaS <kaffeeunused@gmail.com>
2026-01-07 17:25:24 +0700

    Add day 18 final project part 2 file and commented "the colorgram code"

---
+++ Day_18-100/The Hirst Painting Project part 1 & 2/Hirst Painting PART 2.py
@@ -0,0 +1,15 @@
+
+
+# import colorgram
+
+# rgb_colors = []
+# colors = colorgram.extract('D:\downloadrandom\Projects_Udemy_100DaysOfPython_byAngelaYu\Day_18-100\The Hirst Painting Project part 1 & 2\hirst_painting_dots.jpg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+# print(rgb_colors)
+
+actual_colors = [(19, 24, 48), (197, 139, 153), (136, 67, 91), (224, 207, 135), (74, 96, 129), (91, 113, 100), (198, 87, 108), (138, 157, 149), (60, 20, 35), (146, 157, 175), (117, 36, 59), (225, 168, 184), (39, 56, 103), (131, 82, 64), (221, 182, 168), (104, 126, 159), (110, 138, 126), (182, 189, 209), (38, 80, 59), (183, 198, 184), (173, 114, 95), (39, 69, 53), (212, 115, 37), (102, 51, 28), (113, 138, 142)]
