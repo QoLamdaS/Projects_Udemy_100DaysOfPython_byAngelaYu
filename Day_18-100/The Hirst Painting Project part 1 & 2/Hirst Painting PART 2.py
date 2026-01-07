1ee2150bc5f71ed2c70c219ccf67c7c85584d6ee
QoLamdaS <kaffeeunused@gmail.com>
2026-01-07 17:38:10 +0700

    Setup the code for Hirst Painting Final Project Day 18 Part 2

--- Day_18-100/The Hirst Painting Project part 1 & 2/Hirst Painting PART 2.py
+++ Day_18-100/The Hirst Painting Project part 1 & 2/Hirst Painting PART 2.py
@@ -1,5 +1,5 @@
-
-
+import turtle
+import random
 # import colorgram
 
 # rgb_colors = []
@@ -12,4 +12,11 @@
 #     rgb_colors.append(new_color)
 # print(rgb_colors)
 
+turtle.colormode(255)
 actual_colors = [(19, 24, 48), (197, 139, 153), (136, 67, 91), (224, 207, 135), (74, 96, 129), (91, 113, 100), (198, 87, 108), (138, 157, 149), (60, 20, 35), (146, 157, 175), (117, 36, 59), (225, 168, 184), (39, 56, 103), (131, 82, 64), (221, 182, 168), (104, 126, 159), (110, 138, 126), (182, 189, 209), (38, 80, 59), (183, 198, 184), (173, 114, 95), (39, 69, 53), (212, 115, 37), (102, 51, 28), (113, 138, 142)]
+t = turtle.Turtle()
+screen = turtle.Screen()
+screen.setup(width=1920, height=1080, startx=0, starty=0)
+t.speed("fastest")
+
+turtle.mainloop()
