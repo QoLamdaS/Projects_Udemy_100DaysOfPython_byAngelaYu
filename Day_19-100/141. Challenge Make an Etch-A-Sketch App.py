cc57c266ae2fe3f7944462db5382efb58fbec7d3
QoLamdaS <kaffeeunused@gmail.com>
2026-01-09 19:23:28 +0700

    Solved Lecture 141 Challenge make Etch-a-Sketch App. I feel so sleepy and tired bcs insufficient sleep today (less than 6 hours)

--- Day_19-100/141. Challenge Make an Etch-A-Sketch App.py
+++ Day_19-100/141. Challenge Make an Etch-A-Sketch App.py
@@ -2,6 +2,23 @@ import turtle
 
 t = turtle.Turtle()
 screen = turtle.Screen()
-t.forward(100)
+t.speed(0)
+def move_forward():
+    t.forward(20)
+def move_backward():
+    t.backward(20)
+def turn_left():
+    t.left(20)
+def turn_right():
+    t.right(20)
+def clear_drawing():
+    t.clear()
+
+screen.listen()
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+screen.onkey(clear_drawing, "c")
 
 screen.mainloop()
