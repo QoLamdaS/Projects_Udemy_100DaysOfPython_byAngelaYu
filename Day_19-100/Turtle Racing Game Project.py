aa2d7809ff54d262bddd6054f1bf92614c693b67
QoLamdaS <kaffeeunused@gmail.com>
2026-01-10 08:53:47 +0700

    Add setup code before solving Lecture 143 Challenge

--- Day_19-100/Turtle Racing Game Project.py
+++ Day_19-100/Turtle Racing Game Project.py
@@ -1,2 +1,10 @@
+import turtle
 
-print("pppp")
+t = turtle.Turtle(shape="turtle")
+screen = turtle.Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "blue", "green", "yellow", "purple", "orange"]
+
+
+screen.mainloop()
