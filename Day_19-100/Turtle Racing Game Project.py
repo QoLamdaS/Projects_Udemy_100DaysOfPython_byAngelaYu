d0e7b0b93c06807363593b2eb37cc1753bb6bc0b
QoLamdaS <kaffeeunused@gmail.com>
2026-01-10 09:04:08 +0700

    Solved Lecture 143 Day 19 Challenge by my own using for loop

--- Day_19-100/Turtle Racing Game Project.py
+++ Day_19-100/Turtle Racing Game Project.py
@@ -1,10 +1,13 @@
-import turtle
+from turtle import Turtle, Screen
 
-t = turtle.Turtle(shape="turtle")
-screen = turtle.Screen()
+screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "blue", "green", "yellow", "purple", "orange"]
-
+for i in range(6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[i])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=-70 + i*30)
 
 screen.mainloop()
