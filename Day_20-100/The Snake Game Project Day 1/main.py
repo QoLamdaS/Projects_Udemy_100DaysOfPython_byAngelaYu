f4170cf0f316e2ce56623266b4d45bb4c35b9a04
QoLamdaS <kaffeeunused@gmail.com>
2026-01-11 14:57:39 +0700

    Solved Lecture 147 Day 20 Challenge by my own

--- Day_20-100/The Snake Game Project Day 1/main.py
+++ Day_20-100/The Snake Game Project Day 1/main.py
@@ -5,6 +5,11 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("the Snake Game VERSION!!!!!!")
 
-
+all_blocks = []
+for i in range(3):
+    new_block = Turtle(shape="square")
+    new_block.color("white")
+    new_block.goto(x=0 - 20 * i, y=0)
+    all_blocks.append(new_block)
 
 screen.mainloop()
