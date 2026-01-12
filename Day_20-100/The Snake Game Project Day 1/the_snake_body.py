da7b157b2914a66527d5346f8c43642b09e000d0
QoLamdaS <kaffeeunused@gmail.com>
2026-01-12 19:33:01 +0700

    Trying to create class SnakeBody. I have no idea for Lecture 149 Day 20 Challenge. I can't solve this yet.

---
+++ Day_20-100/The Snake Game Project Day 1/the_snake_body.py
@@ -0,0 +1,13 @@
+class SnakeBody:
+    #! ERROR ERROR ERROR ERROR
+    def __init__(self, all_blocks, screen):
+        game_is_on = True
+        while game_is_on:
+            screen.update()
+            time.sleep(0.1)
+            for block_num in range(len(all_blocks) - 1, 0, -1):
+                new_x = all_blocks[block_num - 1].xcor()
+                new_y = all_blocks[block_num - 1].ycor()
+                all_blocks[block_num].goto(x=new_x, y=new_y)
+            all_blocks[0].forward(20)
+    
