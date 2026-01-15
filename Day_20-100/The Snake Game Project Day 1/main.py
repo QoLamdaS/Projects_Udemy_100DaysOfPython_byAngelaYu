78452c0c5699c75b180ad98a19ab1f2925b5cf73
QoLamdaS <kaffeeunused@gmail.com>
2026-01-15 19:38:19 +0700

    Yeay finally I "understand??" Lecture 149 Day 20 solution about OOP def __init__(self): That concept really challenging for me; I understand it "hopefully hehehe".

--- Day_20-100/The Snake Game Project Day 1/main.py
+++ Day_20-100/The Snake Game Project Day 1/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from the_snake_body import SnakeBody
 import time
 
 screen = Screen()
@@ -7,22 +8,12 @@ screen.bgcolor("black")
 screen.title("the Snake Game VERSION!!!!!!")
 screen.tracer(0)
 
-all_blocks = []
-for i in range(3):
-    new_block = Turtle(shape="square")
-    new_block.color("white")
-    new_block.penup()
-    new_block.goto(x=0 - 20 * i, y=0)
-    all_blocks.append(new_block)
+snake = SnakeBody()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for block_num in range(len(all_blocks) - 1, 0, -1):
-        new_x = all_blocks[block_num - 1].xcor()
-        new_y = all_blocks[block_num - 1].ycor()
-        all_blocks[block_num].goto(x=new_x, y=new_y)
-    all_blocks[0].forward(20)
+    snake.move()
 
 screen.mainloop()
--- Day_20-100/The Snake Game Project Day 1/the_snake_body.py
+++ Day_20-100/The Snake Game Project Day 1/the_snake_body.py
@@ -1,15 +1,21 @@
 from turtle import Turtle
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+move_distance = 20
 
 class SnakeBody:
-    #! ERROR ERROR ERROR ERROR
-    def __init__(self, all_blocks, screen):
-        game_is_on = True
-        while game_is_on:
-            screen.update()
-            time.sleep(0.1)
-            for block_num in range(len(all_blocks) - 1, 0, -1):
-                new_x = all_blocks[block_num - 1].xcor()
-                new_y = all_blocks[block_num - 1].ycor()
-                all_blocks[block_num].goto(x=new_x, y=new_y)
-            all_blocks[0].forward(20)
-    
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+    def create_snake(self):
+        for position in starting_positions:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(move_distance)
