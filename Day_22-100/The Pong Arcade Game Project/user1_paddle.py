b59a4d43dcedba3cf7dd1552a431d26741b89818
QoLamdaS <kaffeeunused@gmail.com>
2026-01-18 17:33:31 +0700

    Add an initializer (default object born) for left paddle user.

--- Day_22-100/The Pong Arcade Game Project/user1_paddle.py
+++ Day_22-100/The Pong Arcade Game Project/user1_paddle.py
@@ -4,4 +4,19 @@ class LeftPaddle(Turtle):
     def __init__(self):
         '''Initializes the left paddle object born inheriting from the Turtle class and sets its defaults.'''
         super().__init__()
-        
+        self.penup()
+        self.speed(0)  
+        self.goto(-600, 0)
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+    
+    def left_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+    
+    def left_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+    
+    
