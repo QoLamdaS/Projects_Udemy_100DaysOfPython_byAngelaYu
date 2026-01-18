0484512269e081ed701ee4f7556b3b99bfd03c9d
QoLamdaS <kaffeeunused@gmail.com>
2026-01-18 17:37:51 +0700

    Renaming paddle class file to more "inclusive" and simpler maybe??

--- Day_22-100/The Pong Arcade Game Project/main.py
+++ Day_22-100/The Pong Arcade Game Project/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from user1_paddle import LeftPaddle
 
 #! I finally taking the challenge in Lecture 160 Day 22
 #! which is creating The Pong Arcade Game Project from scratch by myself without watching/peeking Dr. Angela Yu's coding/solutions.
@@ -8,5 +9,12 @@ screen = Screen()
 screen.setup(width=1.0, height=1.0) 
 screen.bgcolor("black")
 screen.title("The Pong Arcade Game by me!!!")
+
+user1 = LeftPaddle()
+
+screen.listen()
+screen.onkey(user1.left_up, "w")
+screen.onkey(user1.left_down, "s")
+
 
 screen.mainloop()
