73997ffafb37b08dcc71421e3173a6173ca957e7
QoLamdaS <kaffeeunused@gmail.com>
2026-01-18 18:05:10 +0700

    Add RightPaddle class inside users_paddle  highly identical to LeftPaddle class. IMPORTANT ERROR: Cannot holding keypresses for left & right paddles simultaneously from player

--- Day_22-100/The Pong Arcade Game Project/main.py
+++ Day_22-100/The Pong Arcade Game Project/main.py
@@ -1,5 +1,5 @@
 from turtle import Screen
-from users_paddle import LeftPaddle
+from users_paddle import LeftPaddle, RightPaddle
 
 #! I finally taking the challenge in Lecture 160 Day 22
 #! which is creating The Pong Arcade Game Project from scratch by myself without watching/peeking Dr. Angela Yu's coding/solutions.
@@ -10,11 +10,12 @@ screen.setup(width=1.0, height=1.0)
 screen.bgcolor("black")
 screen.title("The Pong Arcade Game by me!!!")
 
-user1 = LeftPaddle()
-
 screen.listen()
+user1 = LeftPaddle()
 screen.onkeypress(user1.left_up, "w")
 screen.onkeypress(user1.left_down, "s")
-
+user2 = RightPaddle() #! CRUCIAL BUG: Cannot holding keypresses for left & right paddles simultaneously.
+screen.onkeypress(user2.right_up, "o")
+screen.onkeypress(user2.right_down, "l")
 
 screen.mainloop()
--- Day_22-100/The Pong Arcade Game Project/users_paddle.py
+++ Day_22-100/The Pong Arcade Game Project/users_paddle.py
@@ -18,5 +18,24 @@ class LeftPaddle(Turtle):
     def left_down(self):
         new_y = self.ycor() - 15
         self.goto(self.xcor(), new_y)
+
+class RightPaddle(Turtle):
+    def __init__(self):
+        '''Initializes the right paddle object born inheriting from the Turtle class and sets its defaults.'''
+        super().__init__()
+        self.penup()
+        self.speed(0)  
+        self.goto(600, 0)
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+    
+    def right_up(self):
+        new_y = self.ycor() + 15
+        self.goto(self.xcor(), new_y)
+    
+    def right_down(self):
+        new_y = self.ycor() - 15
+        self.goto(self.xcor(), new_y)
     
     
