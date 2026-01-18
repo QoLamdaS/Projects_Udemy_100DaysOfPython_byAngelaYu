1b3317047db347aefa43f67f33b0aa32da5f23a4
QoLamdaS <kaffeeunused@gmail.com>
2026-01-18 16:07:22 +0700

    Finally taking the challenge creating The Pong Game without watching Dr. Angela coding. WISH ME LUCK TACKLING THIS CHALLENGE!!

---
+++ Day_22-100/The Pong Arcade Game Project/main.py
@@ -0,0 +1,4 @@
+
+#! I finally taking the challenge in Lecture 160 Day 22
+#! which is creating The Pong Arcade Game Project from scratch by myself without watching/peeking Dr. Angela Yu's coding/solutions.
+#* Wish me luck to tackle this challenge by myself!
