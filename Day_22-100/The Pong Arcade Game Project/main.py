1ec4a35a5e8339fa77bc2941771e0dfcf61e5be7
QoLamdaS <kaffeeunused@gmail.com>
2026-01-18 16:09:51 +0700

    Add turtle Screen class as a screen variable

--- Day_22-100/The Pong Arcade Game Project/main.py
+++ Day_22-100/The Pong Arcade Game Project/main.py
@@ -1,4 +1,7 @@
+from turtle import Screen
 
 #! I finally taking the challenge in Lecture 160 Day 22
 #! which is creating The Pong Arcade Game Project from scratch by myself without watching/peeking Dr. Angela Yu's coding/solutions.
 #* Wish me luck to tackle this challenge by myself!
+
+screen = Screen()
