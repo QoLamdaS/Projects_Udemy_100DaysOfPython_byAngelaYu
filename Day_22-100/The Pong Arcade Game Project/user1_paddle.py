63b4b2b1465f3c1e8863f1385fdc4d415fc380d7
QoLamdaS <kaffeeunused@gmail.com>
2026-01-18 17:16:26 +0700

    Add LeftPaddle class file setup inheritance. TRIAL AND ERROR hehehehe

---
+++ Day_22-100/The Pong Arcade Game Project/user1_paddle.py
@@ -0,0 +1,7 @@
+from turtle import Turtle
+
+class LeftPaddle(Turtle):
+    def __init__(self):
+        '''Initializes the left paddle object born inheriting from the Turtle class and sets its defaults.'''
+        super().__init__()
+        
