db68bcf30a827dd6981d3ab3ee5064e87587d3ac
QoLamdaS <kaffeeunused@gmail.com>
2026-01-29 19:30:22 +0700

    Add player turtle object and move_y turtle moveable

--- Day_23-100/Turtle Crossing Game Capstone Project/main.py
+++ Day_23-100/Turtle Crossing Game Capstone Project/main.py
@@ -8,7 +8,13 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+screen.listen()
+user_turtle = Player()
+screen.onkey(user_turtle.move_y, "w")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.mainloop()
--- Day_23-100/Turtle Crossing Game Capstone Project/player.py
+++ Day_23-100/Turtle Crossing Game Capstone Project/player.py
@@ -1,7 +1,19 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        '''Initializes player turtle object born with default settings. (sets its default attributes and methods)'''
+        super().__init__()
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+        self.shape("turtle")
+    
+    def move_y(self):
+        move = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), move)
