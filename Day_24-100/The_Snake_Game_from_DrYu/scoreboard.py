49aa3c90ddf494f05cfdaf1da66e6bd3a12be922
QoLamdaS <kaffeeunused@gmail.com>
2026-01-31 14:26:13 +0700

    Crucial comments to prevent future-error FileNotFoundError: [Errno 2] No such file or directory:

--- Day_24-100/The_Snake_Game_from_DrYu/scoreboard.py
+++ Day_24-100/The_Snake_Game_from_DrYu/scoreboard.py
@@ -8,6 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        #! Use double backslashes instead of single backslash for copy-paste absolute (full) path of a .txt file to avoid VScode's terminal errors
+        #! my ERROR: FileNotFoundError: [Errno 2] No such file or directory: 'D:\downloadrandom\Projects_Udemy_100DaysOfPython_byAngelaYu\Day_24-100\The_Snake_Game_from_DrYu\the_data.txt'
         with open("D:\\downloadrandom\\Projects_Udemy_100DaysOfPython_byAngelaYu\\Day_24-100\\The_Snake_Game_from_DrYu\\the_data.txt", mode="r") as data_score:
             self.high_score = int(data_score.read())
         self.color("white")
@@ -23,6 +25,8 @@ class Scoreboard(Turtle):
     def reset_scores(self):
         if self.score > self.high_score: #* Check if current score is greater than the high score
             self.high_score = self.score
+            #! Use double backslashes instead of single backslash for copy-paste absolute (full) path of a .txt file to avoid VScode's terminal errors
+            #! my ERROR: FileNotFoundError: [Errno 2] No such file or directory: 'D:\downloadrandom\Projects_Udemy_100DaysOfPython_byAngelaYu\Day_24-100\The_Snake_Game_from_DrYu\the_data.txt'
             with open("D:\\downloadrandom\\Projects_Udemy_100DaysOfPython_byAngelaYu\\Day_24-100\\The_Snake_Game_from_DrYu\\the_data.txt", mode="w") as data_score:
                 data_score.write(f"{self.high_score}")
         self.score = 0
