e163a77b250fdfb337e048e5e5824399b05fd84d
QoLamdaS <kaffeeunused@gmail.com>
2026-02-03 18:09:40 +0700

    Finally I completed Day 24 Final Project "Mail Merge" by my own. Honestly I still have no idea :)

--- Day_24-100/Mail Merge Project/Mail Merge Project Start/main.py
+++ Day_24-100/Mail Merge Project/Mail Merge Project Start/main.py
@@ -10,6 +10,12 @@
         
 with open("Day_24-100\\Mail Merge Project\\Mail Merge Project Start\\Input\\Names\\invited_names.txt") as names_file:
 #! Must type double backslashes instead of a single backslash for copy-paste file relative paths on my VScode Windows. I have no idea =)
-    the_names = names_file.readlines()
+    the_names = names_file.readlines() #* Transform from a txt file into a Python list of names
     
-print(the_names)
+with open("Day_24-100\\Mail Merge Project\\Mail Merge Project Start\\Input\\Letters\\starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for i in the_names:
+        stripped_name = i.strip() #* Remove the \n from each name
+        new_letter = letter_contents.replace("[name]", stripped_name) #* Replace the [name] placeholder with the actual intended name
+        with open(f"Day_24-100\\Mail Merge Project\\Mail Merge Project Start\\Output\\ReadyToSend\\letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
