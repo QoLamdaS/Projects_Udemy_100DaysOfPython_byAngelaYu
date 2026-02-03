414e446d95be8cbd0424d648be81197ccbbc8466
QoLamdaS <kaffeeunused@gmail.com>
2026-02-03 17:58:48 +0700

    Finally I successfully solved file relative paths errors. Must use double backslashes!!!

--- Day_24-100/Mail Merge Project/Mail Merge Project Start/main.py
+++ Day_24-100/Mail Merge Project/Mail Merge Project Start/main.py
@@ -7,3 +7,9 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+        
+with open("Day_24-100\\Mail Merge Project\\Mail Merge Project Start\\Input\\Names\\invited_names.txt") as names_file:
+#! Must type double backslashes instead of a single backslash for copy-paste file relative paths on my VScode Windows. I have no idea =)
+    the_names = names_file.readlines()
+    
+print(the_names)
