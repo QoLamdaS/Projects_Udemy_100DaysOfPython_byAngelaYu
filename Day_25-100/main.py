27c9c6a71f60de16d8b9e8288a0ba707c1e7c7e7
QoLamdaS <kaffeeunused@gmail.com>
2026-02-05 17:35:06 +0700

    Solved Lecture 190 Day 25 second challenge

--- Day_25-100/main.py
+++ Day_25-100/main.py
@@ -1,4 +1,14 @@
+import csv
+
+# with open ("Day_25-100\\weather_data.csv") as csv_file:
+#     data = csv_file.readlines()
+#     print(data)
 
 with open ("Day_25-100\\weather_data.csv") as csv_file:
-    data = csv_file.readlines()
-    print(data)
+    data = csv.reader(csv_file)
+    temp = []
+    for row in data:
+        if row[1] != "temp":
+            temp.append(int(row[1]))
+    print(temp)
+
