cfc7ac0bd04a2151eac9df2c7a0a8d9c831c73b5
QoLamdaS <kaffeeunused@gmail.com>
2026-02-06 19:21:34 +0700

    Finished Lecture 190 Day 25

--- Day_25-100/main.py
+++ Day_25-100/main.py
@@ -1,14 +1,18 @@
-import csv
+import pandas
+# import csv
 
 # with open ("Day_25-100\\weather_data.csv") as csv_file:
 #     data = csv_file.readlines()
 #     print(data)
 
-with open ("Day_25-100\\weather_data.csv") as csv_file:
-    data = csv.reader(csv_file)
-    temp = []
-    for row in data:
-        if row[1] != "temp":
-            temp.append(int(row[1]))
-    print(temp)
+# with open ("Day_25-100\\weather_data.csv") as csv_file:
+#     data = csv.reader(csv_file)
+#     temp = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temp.append(int(row[1]))
+#     print(temp)
 
+data = pandas.read_csv("Day_25-100\\weather_data.csv")
+print(data)
+print(data["temp"])
