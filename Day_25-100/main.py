b3ea5a003648dc224b70353b863e8aef08874b17
QoLamdaS <kaffeeunused@gmail.com>
2026-02-07 14:25:35 +0700

    Add starting code before tackling first challenge of Lecture 191 Day 25

--- Day_25-100/main.py
+++ Day_25-100/main.py
@@ -1,18 +1,8 @@
 import pandas
-# import csv
-
-# with open ("Day_25-100\\weather_data.csv") as csv_file:
-#     data = csv_file.readlines()
-#     print(data)
-
-# with open ("Day_25-100\\weather_data.csv") as csv_file:
-#     data = csv.reader(csv_file)
-#     temp = []
-#     for row in data:
-#         if row[1] != "temp":
-#             temp.append(int(row[1]))
-#     print(temp)
 
 data = pandas.read_csv("Day_25-100\\weather_data.csv")
-print(data)
-print(data["temp"])
+data_dict = data.to_dict()
+print(data_dict)
+temp_list = data["temp"].to_list()
+print(temp_list)
+
