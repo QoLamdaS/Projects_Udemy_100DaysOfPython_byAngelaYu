1067d0d787b81b5b9fe01e79c61f7ecbfb9621a8
QoLamdaS <kaffeeunused@gmail.com>
2026-02-07 15:36:57 +0700

    Solved Lecture 192 Day 25 challenge by my own. Quite challenging for me

--- Day_25-100/The Great Squirrel Census Data Analysis in Lecture 192/main.py
+++ Day_25-100/The Great Squirrel Census Data Analysis in Lecture 192/main.py
@@ -1,2 +1,13 @@
+import pandas
 
-print("ff")
+data = pandas.read_csv("Day_25-100\\The Great Squirrel Census Data Analysis in Lecture 192\\2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+squirrels_csv_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+the_data_frame = pandas.DataFrame(squirrels_csv_dict)
+the_data_frame.to_csv("Day_25-100\\The Great Squirrel Census Data Analysis in Lecture 192\\final_squirrel_count.csv")
+
