b5ebafe73c63bc7032e0e2876e2efbc828c29c59
QoLamdaS <kaffeeunused@gmail.com>
2026-02-08 12:06:57 +0700

    15% finished Lecture 194 Day 25 challenge by my own. I have no idea

--- Day_25-100/U.S. States Game Project/main.py
+++ Day_25-100/U.S. States Game Project/main.py
@@ -5,8 +5,17 @@ screen = turtle.Screen()
 screen.title("U.S. States Game by me!!! YEAh!!")
 screen.addshape("Day_25-100\\U.S. States Game Project\\blank_states_img.gif")
 turtle.shape("Day_25-100\\U.S. States Game Project\\blank_states_img.gif")
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
 
-input_state = screen.textinput(title="Guess the State???", prompt="What is state's name?").title()
-print(input_state)
+data = pandas.read_csv("Day_25-100\\U.S. States Game Project\\50_states.csv")
+all_us_states = data.state.to_list()
+
+is_game_on = True
+while is_game_on:
+    input_state = screen.textinput(title="Guess the State???", prompt="What is state's name?").title()
+    if input_state in all_us_states:
+        
 
 screen.mainloop()
