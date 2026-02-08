cd9d9fb601238430316cff6b1acb8900df8a59db
QoLamdaS <kaffeeunused@gmail.com>
2026-02-08 13:25:50 +0700

    Solved Lecture 194 Day 25 challenge by my own unique solution. Quite challenging for me. I have no idea why using .item() necessary to work for coordinates

--- Day_25-100/U.S. States Game Project/main.py
+++ Day_25-100/U.S. States Game Project/main.py
@@ -10,12 +10,15 @@ t.hideturtle()
 t.penup()
 
 data = pandas.read_csv("Day_25-100\\U.S. States Game Project\\50_states.csv")
-all_us_states = data.state.to_list()
+remaining_all_states = data.state.to_list()
+score = 0
 
-is_game_on = True
-while is_game_on:
-    input_state = screen.textinput(title="Guess the State???", prompt="What is state's name?").title()
-    if input_state in all_us_states:
-        
+while score < 50:
+    input_state = screen.textinput(title=f"{score}/{len(data.state)} U.S. States Correct", prompt="What is state's name?").title()
+    if input_state in remaining_all_states:
+        score += 1
+        remaining_all_states.remove(input_state)
+        t.goto(data.x[data.state == input_state].item(), data.y[data.state == input_state].item()) #* actually I don't know why using .item() is needed here lol 
+        t.write(input_state)
 
 screen.mainloop()
