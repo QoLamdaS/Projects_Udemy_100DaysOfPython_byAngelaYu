315951d167fa1e00c5130c307af9a8b5ddb761a4
QoLamdaS <kaffeeunused@gmail.com>
2026-02-14 17:17:58 +0700

    Solved Coding Exercise 17 Day 26. Quite challenging

--- Day_26-100/Coding Exercise 17 Data Overlap/exercise.py
+++ Day_26-100/Coding Exercise 17 Data Overlap/exercise.py
@@ -1,6 +1,9 @@
 
+with open("Day_26-100\\Coding Exercise 17 Data Overlap\\file1.txt") as file1:
+    file1_string_list = file1.readlines()
+with open("Day_26-100\\Coding Exercise 17 Data Overlap\\file2.txt") as file2:
+    file2_string_list = file2.readlines()
 
-
-result = 
+result = [int(i) for i in file1_string_list if i in file2_string_list] 
 
 print(result)
