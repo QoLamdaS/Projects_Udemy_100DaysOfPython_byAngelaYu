a4da1f58dcf7bd7679e310eebf18cf4b162b4bca
QoLamdaS <kaffeeunused@gmail.com>
2026-02-14 16:45:23 +0700

    Solved Coding Exercise 16 by myself a bit harder for this.

--- Day_26-100/Coding Exercise 16 Filtering Even Numbers.py
+++ Day_26-100/Coding Exercise 16 Filtering Even Numbers.py
@@ -1,4 +1,4 @@
 list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
-numbers = 
-result = 
-print(result)
+numbers_to_int = [int(i) for i in list_of_strings]
+even_numbers = [i for i in numbers_to_int if i % 2 == 0]
+print(even_numbers)
