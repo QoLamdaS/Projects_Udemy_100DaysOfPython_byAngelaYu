9f24d0990e677eb4f91fffb415e2d57df9214c78
QoLamdaS <kaffeeunused@gmail.com>
2026-02-15 14:38:37 +0700

    Solved Coding Exercise 19 Day 26 by myself quite easily again

--- Day_26-100/Coding Exercise 19 Dictionary Comprehension 2.py
+++ Day_26-100/Coding Exercise 19 Dictionary Comprehension 2.py
@@ -1,5 +1,5 @@
 weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
 
-weather_f = 
+weather_f = {day_key:(temp_value * 9/5) + 32 for (day_key, temp_value) in weather_c.items()}
 
 print(weather_f)
