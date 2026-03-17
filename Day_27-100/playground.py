9fd4fab027f77999d7d19a27c692461a09028f59
QoLamdaS <kaffeeunused@gmail.com>
2026-03-17 18:30:40 +0700

    add starting code before tackling Lecture 209 Day 27 first challenge.

--- Day_27-100/playground.py
+++ Day_27-100/playground.py
@@ -1,8 +1,18 @@
+import tkinter
 
-def add(*args):
-    print(args[2])
-    for i in args:
-        print(i * i)
+window = tkinter.Tk()
+window.title("soo First GUI Program")
+window.minsize(width=500, height=300)
 
-add(19, 20, 21, 22, 23)
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+def button_clicked():
+    print("I got clicked")
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+window.mainloop()
 
