101c617f6a153b45b9bd9540831f92fbaad2d84b
QoLamdaS <kaffeeunused@gmail.com>
2026-03-17 18:34:47 +0700

    Solved Lecture 209 Day 27 first challenge by myself relatively easy

--- Day_27-100/playground.py
+++ Day_27-100/playground.py
@@ -1,16 +1,17 @@
 import tkinter
 
 window = tkinter.Tk()
-window.title("soo First GUI Program")
+window.title("soo ori First GUI Program idk")
 window.minsize(width=500, height=300)
 
 my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
-my_label["text"] = "New Text"
 my_label.config(text="New Text")
 
 def button_clicked():
     print("I got clicked")
+    my_label.config(text="Button got clicked")
+
 button = tkinter.Button(text="Click Me", command=button_clicked)
 button.pack()
 
