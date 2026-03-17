c94618dd38882f7a606d1f4b3e0b05b49df1cf47
QoLamdaS <kaffeeunused@gmail.com>
2026-03-17 18:42:49 +0700

    Solved Lecture 209 Day 27 second challenge. a bit harder =)

--- Day_27-100/playground.py
+++ Day_27-100/playground.py
@@ -9,11 +9,13 @@ my_label.pack()
 my_label.config(text="New Text")
 
 def button_clicked():
-    my_label.config(text="Button got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
 
 button = tkinter.Button(text="Click Me", command=button_clicked)
 button.pack()
 input = tkinter.Entry(width=10)
+input.insert(0, "Some text to begin with.")
 input.pack()
 
 
