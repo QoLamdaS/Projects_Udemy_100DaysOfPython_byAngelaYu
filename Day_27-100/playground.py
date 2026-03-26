698de91b7dae23155dffddaf82fcb2598f38dee8
QoLamdaS <kaffeeunused@gmail.com>
2026-03-26 18:33:20 +0700

    Tidy up the starter code Lecture 211 Day 27 before solving the first challenge for this lecture

--- Day_27-100/playground.py
+++ Day_27-100/playground.py
@@ -3,21 +3,24 @@ import tkinter
 window = tkinter.Tk()
 
 window.title("My First GUI Program")
-window.minsize(width=508, height=368)
+window.minsize(width=500, height=300)
+
+def button_clicked():
+    my_label.config(text=input.get())
 
 #Label
-
-my_label = Lael (ey = Am a Label", font=("Arial®, 24, "bold"))
+my_label = tkinter.Label(text = "I am a label", font=("Arial", 24, "bold"))
 my_label.config(text="New Text")
-
-my_label.grid(-olunn=8, row=8)
+my_label.grid(column=0, row=0)
 
 #Button
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
 
-button = Button(text="Click Me", connand=button_clicked)
 #Entry
-input = Entry(width=18)
+input = tkinter.Entry(width=18)
 print(input.get())
+input.grid(column=2, row=2)
 
 window.mainloop()
 
