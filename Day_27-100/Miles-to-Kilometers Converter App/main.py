4e35a51501eaa44c111ec40328b16c27cd22c31d
QoLamdaS <kaffeeunused@gmail.com>
2026-03-31 18:41:00 +0700

    Finally completed Day 27 Final Project by myself. FInished Day 27 Course finally. I feel a bit exhausted and sleepy

--- Day_27-100/Miles-to-Kilometers Converter App/main.py
+++ Day_27-100/Miles-to-Kilometers Converter App/main.py
@@ -6,4 +6,23 @@ window.config(padx=20, pady=20)
 def miles_to_km():
     miles = float(miles_input.get())
     km = round(miles * 1.609)
-    
+    result_label.config(text=km)
+result_label = tkinter.Label(text="0")
+result_label.grid(column=1, row=1)
+
+miles_input = tkinter.Entry(width=10)
+miles_input.grid(column=1, row=0)
+
+miles_label = tkinter.Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_label = tkinter.Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+km_label = tkinter.Label(text="Km")
+km_label.grid(column=2, row=1)
+
+calculate_button = tkinter.Button(text="Calculate", command=miles_to_km)
+calculate_button.grid(column=1, row=2)
+
+window.mainloop()
