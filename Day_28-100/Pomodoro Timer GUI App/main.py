9d1b1081400c4a60a90666cb1ee3dd860d20d57f
QoLamdaS <kaffeeunused@gmail.com>
2026-04-02 12:06:10 +0700

    Add starter code for Day 28 final project "Pomodoro App"

---
+++ Day_28-100/Pomodoro Timer GUI App/main.py
@@ -0,0 +1,24 @@
+import tkinter
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("Tomat timer")
+
+canva = tkinter.Canvas(width=200, height=224)
+tomato_image = tkinter.PhotoImage(file="Day_28-100\\Pomodoro Timer GUI App\\tomato.png")
