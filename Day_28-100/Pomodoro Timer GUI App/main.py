131886cb087ed808478e44fac5633d4dd7644ea1
QoLamdaS <kaffeeunused@gmail.com>
2026-04-10 17:22:44 +0700

    Finished watching Lecture 214 Day 28. Feel so lazy and unmotivated

--- Day_28-100/Pomodoro Timer GUI App/main.py
+++ Day_28-100/Pomodoro Timer GUI App/main.py
@@ -21,9 +21,10 @@ window = tkinter.Tk()
 window.title("Tomat timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canva = tkinter.Canvas(width=200, height=224)
+canva = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = tkinter.PhotoImage(file="Day_28-100\\Pomodoro Timer GUI App\\tomato.png")
-canva.create_image(103, 112, image=tomato_image)
+canva.create_image(100, 112, image=tomato_image)
+canva.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canva.pack()
 
 
