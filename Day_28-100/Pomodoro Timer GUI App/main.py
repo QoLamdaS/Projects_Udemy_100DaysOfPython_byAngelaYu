c0fc2b3170840ab3c69d0e3f232e2f5b4bba84e8
QoLamdaS <kaffeeunused@gmail.com>
2026-04-15 20:12:07 +0700

    Solved Lecture 215 Day 28 challenge by myself for Pomodoro Timer GUI App

--- Day_28-100/Pomodoro Timer GUI App/main.py
+++ Day_28-100/Pomodoro Timer GUI App/main.py
@@ -30,8 +30,14 @@ canva.grid(column=1, row=1)
 timer_label = tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 timer_label.grid(column=1, row=0)
 
+checkmarks_label = tkinter.Label(text="✔", fg=GREEN, bg=YELLOW)
+checkmarks_label.grid(column=1, row=3)
 
+start_button = tkinter.Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
 
+reset_button = tkinter.Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
 
 window.mainloop()
 
