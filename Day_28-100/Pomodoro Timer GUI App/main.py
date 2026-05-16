5289b98cdc6b4c93e8ac0916447e446898be1e65
QoLamdaS <kaffeeunused@gmail.com>
2026-05-16 08:50:12 +0700

    Okay time to solve now

--- Day_28-100/Pomodoro Timer GUI App/main.py
+++ Day_28-100/Pomodoro Timer GUI App/main.py
@@ -13,7 +13,8 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    count_down(5)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     canva.itemconfig(timer_text, text=count)
@@ -31,7 +32,7 @@ canva.create_image(100, 112, image=tomato_image)
 timer_text = canva.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canva.grid(column=1, row=1)
 
-count_down(5)
+
 
 timer_label = tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 timer_label.grid(column=1, row=0)
