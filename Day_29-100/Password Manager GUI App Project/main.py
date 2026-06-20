5a3f506b17db8e4e8d53acf9c646ef796b56e93e
QoLamdaS <kaffeeunused@gmail.com>
2026-06-20 16:25:28 +0700

    Solved the first challenge of Lecture 221 Day 29 working Password Manager GUI App project

--- Day_29-100/Password Manager GUI App Project/main.py
+++ Day_29-100/Password Manager GUI App Project/main.py
@@ -5,5 +5,17 @@ import tkinter
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = tkinter.Tk()
+window.title("THE PassWord Manager")
+window.config(padx=50, pady=50)
+
+canva = tkinter.Canvas(width=200, height=200)
+logo_img = tkinter.PhotoImage(file="Day_29-100\\Password Manager GUI App Project\\logo.png")
+canva.create_image(100, 100, image=logo_img)
+canva.grid(column=1, row=0)
+
+
+
+window.mainloop()
 
 
