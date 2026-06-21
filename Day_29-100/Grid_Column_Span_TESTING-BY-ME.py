4456b84e897a1d756d80d5a22cb46ea8e2e70240
QoLamdaS <kaffeeunused@gmail.com>
2026-06-21 17:15:49 +0700

    I am trying to understand by experimenting

--- Day_29-100/Grid_Column_Span_TESTING-BY-ME.py
+++ Day_29-100/Grid_Column_Span_TESTING-BY-ME.py
@@ -8,7 +8,7 @@ r.grid(row=0, column=0)
 g = tkinter.Label(bg="green", width=20, height=5)
 g.grid(row=1, column=1)
 
-b = tkinter.Label(bg="blue", width=40, height=5)
+b = tkinter.Label(bg="blue", width=20, height=5)
 b.grid(row=2, column=0, columnspan=2)
 
 
