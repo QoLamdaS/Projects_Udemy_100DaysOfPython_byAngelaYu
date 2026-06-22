72a164fb1b69f435c6509618ebfe0b6a3f14959c
QoLamdaS <kaffeeunused@gmail.com>
2026-06-22 17:14:27 +0700

    Experimenting to solve Lecture 222 Day 29 challenge by myself.

--- Day_29-100/Grid_Column_Span_TESTING-BY-ME.py
+++ Day_29-100/Grid_Column_Span_TESTING-BY-ME.py
@@ -2,14 +2,14 @@ import tkinter
 
 window = tkinter.Tk()
 
-r = tkinter.Label(bg="red", width=20, height=5)
+r = tkinter.Label(bg="red", width=40, height=5)
 r.grid(row=0, column=0)
 
-g = tkinter.Label(bg="green", width=20, height=5)
+g = tkinter.Label(bg="green", width=40, height=5)
 g.grid(row=1, column=1)
 
-b = tkinter.Label(bg="blue", width=20, height=5)
-b.grid(row=2, column=0, columnspan=2)
+b = tkinter.Label(bg="blue", width=80, height=5)
+b.grid(row=2, column=0, columnspan=1)
 
 
 window.mainloop()
